b66ca5c6938d64b796cac0c360e563576120f4e4
luizxbertalha <luizbertalha@gmail.com>
2025-01-29 20:03:50 -0300

    Add files via upload
    Implementada a função de atualizar a lista de amigos e realizada algumas correções para melhor leitura. Acrescentada a função de apertar Enter para atualizar a lista de amigos.

--- Amigo Secreto.js
+++ Amigo Secreto.js
@@ -10,19 +10,24 @@ function adicionarAmigos() {
         alert('Por favor, insira um nome.');
     } else {
         amigos.push(nomeAmigo);
-        
         inputAmigo.value = '';
-
         console.log(amigos);
+        exibirAmigos();
     }
 }
-document.querySelector('.button-add').addEventListener('click', adicionarAmigo);
-
+document.querySelector('.button-add').addEventListener('click', adicionarAmigos);
 document.getElementById('amigo').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         adicionarAmigos(); 
     }
 });
+function exibirAmigos() {
+    let lista = document.getElementById('Amigos');
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let itemLista = document.createElement('li');
+        itemLista.textContent = amigos[i];
+        lista.appendChild(itemLista);
+    }
+}
 
-
-    
